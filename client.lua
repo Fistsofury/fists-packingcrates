811184cec08dd64ee77a589056bf28b0167b361f
Fistsofury <92045818+Fistsofury@users.noreply.github.com>
2024-02-25 12:58:30 +0000

    Initial commit

--- client.lua
+++ client.lua
@@ -1,2 +1,77 @@
-FeatherMenu =  exports['feather-menu'].initiate()
 
+progressbar = exports.vorp_progressbar:initiate()
+
+RegisterNetEvent('fists-packingcrates:crateItems', function(craftableItems)
+    local FeatherMenu = exports['feather-menu'].initiate()
+    local packagingMenu = FeatherMenu:RegisterMenu('packagingMenu', {
+        top = '40%',
+        left = '20%',
+        ['720width'] = '500px',
+        ['1080width'] = '600px',
+        draggable = true,
+    })
+    
+    local packagingPage = packagingMenu:RegisterPage('packagingOptions')
+    packagingPage:RegisterElement('header', {value = 'Packaging Options', slot = "header"})
+
+    for _, item in ipairs(craftableItems) do
+        packagingPage:RegisterElement('button', {
+            label = "package " .. item.label,
+            slot = "content",
+            id = item.name,
+        }, function()
+            TriggerEvent('fists-crates:StartPackingAnimation', item.name)
+            Wait(8000)
+            TriggerServerEvent('crafting:craftItem', item.name)
+        end)
+    end
+    
+    packagingMenu:Open({startupPage = packagingPage})
+end)
+
+
+
+RegisterNetEvent('fists-crates:StartCraftingAnimation', function(crateLabel)
+    local playerPed = PlayerPedId()
+    local coords = GetEntityCoords(playerPed)
+    
+    -- Start the progress bar
+    progressbar.start('Unpacking ' .. crateLabel .. '...', 8000, function()
+    end, 'innercircle')
+    
+    -- Play the animation
+    local animDict = "amb_work@world_human_hammer_kneel_stakes@male@male_a@base"
+    local animName = "base"
+    
+    RequestAnimDict(animDict)
+    while not HasAnimDictLoaded(animDict) do
+        Wait(100)
+    end
+    
+    TaskPlayAnim(playerPed, animDict, animName, 8.0, -8.0, -1, 49, 0, false, false, false)
+    Wait(8000) -- Wait for the duration of the progress bar
+    ClearPedTasks(playerPed)
+end)
+
+
+RegisterNetEvent('fists-crates:StartPackingAnimation', function(crateLabel)
+    local playerPed = PlayerPedId()
+    local coords = GetEntityCoords(playerPed)
+    
+    -- Start the progress bar
+    progressbar.start('Packing ' .. crateLabel .. '...', 8000, function()
+    end, 'innercircle')
+    
+    -- Play the animation
+    local animDict = "amb_work@world_human_hammer_kneel_stakes@male@male_a@base"
+    local animName = "base"
+    
+    RequestAnimDict(animDict)
+    while not HasAnimDictLoaded(animDict) do
+        Wait(100)
+    end
+    
+    TaskPlayAnim(playerPed, animDict, animName, 8.0, -8.0, -1, 49, 0, false, false, false)
+    Wait(8000) -- Wait for the duration of the progress bar
+    ClearPedTasks(playerPed)
+end)
--- server.lua
+++ server.lua
@@ -1,18 +1,62 @@
 local VORPcore = exports.vorp_core:GetCore()
 
-function registerCrate()
-    exports.vorp_inventory:registerUsableItem(config.emptyCrate, function(data)
-        TriggerClientEvent("fists-crates:openCrate", data._source, config.crates)
+
+
+-- Function to check player's inventory and send craftable options
+local function checkCraftableItems(source)
+    local _source = source
+    local craftableItems = {}
+    exports.vorp_inventory:getUserInventoryItems(_source, function(inventory)
+        for _, crate in ipairs(config.crates) do
+            local itemAmount = exports.vorp_inventory:getItemCount(_source, nil, crate.requiredItem, nil)
+            if itemAmount >= crate.requiredQuantity then
+                table.insert(craftableItems, crate)
+            end
+        end
+        TriggerClientEvent("fists-packingcrates:crateItems", source, craftableItems)
     end)
 end
 
+-- Register empty crate as usable and check craftable items when used
+exports.vorp_inventory:registerUsableItem(config.emptyCrate, function(data)
+    checkCraftableItems(data.source)
+end)
 
+-- Crafting event
+RegisterServerEvent("crafting:craftItem")
+AddEventHandler("crafting:craftItem", function(crateName)
+    local _source = source
+    for _, crate in ipairs(config.crates) do
+        if crate.name == crateName then
+            local itemAmount = exports.vorp_inventory:getItemCount(_source, nil, crate.requiredItem, nil)
+            if itemAmount >= crate.requiredQuantity then
+                -- Subtract required items and empty crate, add crafted crate
+                exports.vorp_inventory:subItem(_source, crate.requiredItem, crate.requiredQuantity, nil)
+                exports.vorp_inventory:subItem(_source, config.emptyCrate, 1, nil)
+                exports.vorp_inventory:addItem(_source, crateName, 1, nil)
+                TriggerClientEvent("vorp:TipBottom", _source, "Crafted: " .. crate.label, 4000)
+                checkCraftableItems(_source)
+            else
+                TriggerClientEvent("vorp:TipBottom", _source, "Not enough items to craft: " .. crate.label, 4000)
+            end
+            break
+        end
+    end
+end)
 
-
-
-
-
-
-
-
-exports.vorp_inventory:registerUsableItem(item, callback)
+for _, crate in ipairs(config.crates) do
+    exports.vorp_inventory:registerUsableItem(crate.name, function(data)
+        local _source = data.source
+        crateCount = exports.vorp_inventory:getItemCount(_source, nil, crate.name)
+        if crateCount >= 1 then
+            TriggerClientEvent('fists-crates:StartCraftingAnimation', _source, crate.label)
+            Wait(8000)
+            exports.vorp_inventory:addItem(_source, crate.requiredItem, crate.requiredQuantity)
+            exports.vorp_inventory:addItem(_source, config.emptyCrate, 1)
+            exports.vorp_inventory:subItem(_source, crate.name, 1)
+            TriggerClientEvent("vorp:TipRight", _source, "You have created a " .. crate.label, 3000)
+        else
+            TriggerClientEvent("vorp:TipRight", _source, "You don't have enough items to create a " .. crate.label, 3000)
+        end
+    end)
+end
